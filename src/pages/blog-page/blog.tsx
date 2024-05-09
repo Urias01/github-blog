a7d1bd7be14b1f39d561d2e1919d3d667086ea6d
Urias Pereira <uriasluis64@gmail.com>
2024-05-08 21:15:24 -0300

    search issues

--- src/pages/blog-page/blog.tsx
+++ src/pages/blog-page/blog.tsx
@@ -20,6 +20,10 @@ const gitHubProfileResposne = z.object({
 
 export type GitHubprofileResponse = z.infer<typeof gitHubProfileResposne>
 
+interface GitHubIssuesResponse {
+  data: GitHubIssues
+}
+
 const gitHubIssues = z.object({
   total_count: z.coerce.number(),
   items: z.array(
@@ -38,6 +42,7 @@ type GitHubIssues = z.infer<typeof gitHubIssues>
 export function Blog() {
   const [user, setUser] = useState({} as GitHubprofileResponse)
   const [issues, setIssues] = useState<GitHubIssues>({} as GitHubIssues)
+  const [searchIssue, setSearchIssue] = useState<string>('')
 
   async function getUser()  {
     const newUser = await api.get<GitHubprofileResponse>('/users/Urias01')
@@ -53,8 +58,8 @@ export function Blog() {
 
   async function searchIssues(searchBody: string) {
     const foundIssues = await api.get<GitHubIssues>(
-      `/search/issues?q=${searchBody}repo:Urias01/github-blog`)
-      .then((response) => { return response.data })
+      `/search/issues?q=${searchBody} repo:Urias01/github-blog`)
+      .then((response: GitHubIssuesResponse) => { return response.data })
 
     setIssues(foundIssues)
   }
@@ -63,23 +68,37 @@ export function Blog() {
     searchIssues('')
   }, [])
 
+  function handleSearchIssues(event: React.ChangeEvent<HTMLInputElement>) {
+    event.preventDefault()
+    setSearchIssue(event.target.value)
+  }
+
+  function handleSubmitSearchIssues(event: React.ChangeEvent<HTMLFormElement>) {
+    event.preventDefault()
+    searchIssues(searchIssue)
+  }
+
   return (
     <>
     {user && (<BlogProfile user={user} />)}
 
-       <div className="flex flex-col gap-4 w-[884px] self-center">
+      <div className="flex flex-col gap-4 w-[884px] self-center">
         <div className="flex justify-between">
           <h2 className="text-baseSubtitle text-xl font-bold">Publicações</h2>
           <p className="text-baseSpan">{issues.total_count} publicações</p>
         </div>
-        <input 
-          type="text"
-          id="seachIssues"
-          placeholder="Buscar Conteúdo"
-          className="bg-baseInput placeholder:text-baseLabel border
-           border-baseBorder rounded-md p-4 text-baseText"
-        />
-       </div>
+        <form onSubmit={handleSubmitSearchIssues}>
+          <input 
+            type="text"
+            id="seachIssues"
+            placeholder="Buscar Conteúdo"
+            onChange={handleSearchIssues}
+            className="bg-baseInput placeholder:text-baseLabel border
+            border-baseBorder rounded-md p-4 text-baseText w-full"
+          />
+          <button type="submit" className="hidden"/>
+        </form>
+      </div>
 
        <section className="grid grid-cols-2 w-[884px] self-center mt-8 gap-8 mb-8">
         {Array.isArray(issues.items) && (
